804aac9385084265b34733711b1ba928a11f6f41
robaho <rengels@ix.netcom.com>
2021-01-01 19:02:27 -0600

    immediately unmap mapped files when closed

--- src/com/robaho/jkeydb/MemoryMappedFile.java
+++ src/com/robaho/jkeydb/MemoryMappedFile.java
@@ -1,15 +1,45 @@
 package com.robaho.jkeydb;
 
+import sun.misc.Unsafe;
+
 import java.io.IOException;
 import java.io.RandomAccessFile;
+import java.lang.reflect.Field;
 import java.nio.ByteBuffer;
 import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
+
+final class UnsafeUtils {
+    private static final Unsafe unsafe;
+
+    public static Unsafe getUnsafe() {
+        return unsafe;
+    }
+
+    static {
+        Field f;
+        try {
+            f = Unsafe.class.getDeclaredField("theUnsafe");
+        } catch (NoSuchFieldException e) {
+            throw new IllegalStateException("unable to get Unsafe",e);
+        }
+
+        f.setAccessible(true);
+
+        try {
+            unsafe = (Unsafe)f.get((Object)null);
+        } catch (IllegalAccessException e) {
+            throw new IllegalStateException("unable to get Unsafe",e);
+        }
+    }
+}
 
 public class MemoryMappedFile {
     private final int MAX_MAP_SIZE = 1024*1024*1024; // 1GB
     private final long length;
     private final MappedByteBuffer[] buffers;
+    private boolean closed;
+
     public MemoryMappedFile(RandomAccessFile file) throws IOException {
         this.length = file.length();
         FileChannel ch = file.getChannel();
@@ -24,7 +54,10 @@ public class MemoryMappedFile {
         return length;
     }
 
-    public void readAt(ByteBuffer bbuffer, long position) {
+    public void readAt(ByteBuffer bbuffer, long position) throws IOException {
+        if(closed)
+            throw new IOException("memory mapped file is closed");
+
        while(bbuffer.remaining()>0) {
            MappedByteBuffer b = buffers[(int) (position / MAX_MAP_SIZE)];
            byte[] buffer = bbuffer.array();
@@ -38,6 +71,9 @@ public class MemoryMappedFile {
     }
 
     public void close() {
-
+        for(MappedByteBuffer buffer : buffers) {
+            UnsafeUtils.getUnsafe().invokeCleaner(buffer);
+        }
+        closed = true;
     }
 }
